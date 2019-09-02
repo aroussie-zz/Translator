83da09e7c51b8be13c04656916300e309745821f
Alex Roussiere <alex.roussiere@snapsheet.me>
2019-09-02 17:44:24 -0500

    Can delete an item with a nice animation

--- lib/models/Translation.dart
+++ lib/models/Translation.dart
@@ -8,8 +8,8 @@ class Translation {
   Translation({this.originalSentence, this.translatedSentence,
       this.originalLanguage, this.translationLanguage});
 
-  Translation.dummy(){
-    this.originalSentence = "Je suis l'originale";
+  Translation.dummy(int randomNumber){
+    this.originalSentence = "Je suis l'originale $randomNumber";
     this.translatedSentence = "I'm the original";
     this.originalLanguage = "French";
     this.translationLanguage = "English";
--- lib/screens/HomeListPage.dart
+++ lib/screens/HomeListPage.dart
@@ -8,17 +8,28 @@ class HomeListPage extends StatefulWidget {
   State<StatefulWidget> createState() {
     return _HomeListState();
   }
-
 }
 
 class _HomeListState extends State<HomeListPage> {
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey();
 
+  //IMPLEMENT SQLITE INSTEAD
   List<Translation> _translations = [
-    Translation.dummy(),
-    Translation.dummy(),
-    Translation.dummy(),
-    Translation.dummy(),
-    Translation.dummy()
+    Translation.dummy(1),
+    Translation.dummy(2),
+    Translation.dummy(3),
+    Translation.dummy(4),
+    Translation.dummy(5),
+    Translation.dummy(6),
+    Translation.dummy(7),
+    Translation.dummy(8),
+    Translation.dummy(9),
+    Translation.dummy(10),
+    Translation.dummy(11),
+    Translation.dummy(12),
+    Translation.dummy(13),
+    Translation.dummy(14),
+    Translation.dummy(15)
   ];
 
   @override
@@ -28,23 +39,43 @@ class _HomeListState extends State<HomeListPage> {
         title: Text("My Translations"),
       ),
       body: SafeArea(
-          child: ListView.builder(
-              itemCount: _translations.length,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  title: Text(_translations[index].originalSentence),
-                  subtitle: Text(_translations[index].translatedSentence),
-                  trailing: Icon(Icons.delete, color: Colors.red),
-                );
-              }
-          )),
+          child: AnimatedList(
+              key: _listKey,
+              initialItemCount: _translations.length,
+              itemBuilder: (context, index, animation) {
+                return _buildListTile(_translations[index], animation);
+              })),
       floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => TranslatePage()));
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => TranslatePage()));
           }),
     );
   }
 
+  ///Build an item with animation that will be given by the AnimatedList
+  Widget _buildListTile(Translation translation, Animation animation) {
+    return SizeTransition(
+        sizeFactor: animation,
+        child: ListTile(
+            title: Text(translation.originalSentence),
+            subtitle: Text(translation.translatedSentence),
+            trailing: IconButton(
+                icon: Icon(Icons.delete, color: Colors.red),
+                onPressed: () {
+                  _deleteListItem(translation);
+                })));
+  }
 
+  ///Delete an item with a nice animation
+  void _deleteListItem(Translation itemToDelete) {
+    int indexToDelete = _translations.indexOf(itemToDelete);
+    _translations.remove(itemToDelete);
+    AnimatedListRemovedItemBuilder builder = (context, animation) {
+      return _buildListTile(itemToDelete, animation);
+    };
+
+    _listKey.currentState.removeItem(indexToDelete, builder);
+  }
 }

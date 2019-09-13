dcc0414b8bf7587dc2aaac2a84da28a8f3e78a77
Alex Roussiere <alex.roussiere@snapsheet.me>
2019-09-13 15:19:50 -0500

    Using Provider to pass the preferences for language

---
+++ lib/models/PreferencesModel.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'Language.dart';
+
+class PreferencesModel extends ChangeNotifier{
+
+  Language originalLanguage;
+  Language translatedLanguage;
+
+  PreferencesModel.FrenchToEnglish(){
+    this.originalLanguage = Language(isoCode: "fr", name: "French");
+    this.translatedLanguage = Language(isoCode: "en", name: "English");
+  }
+
+  PreferencesModel({this.originalLanguage, this.translatedLanguage});
+
+  void updateOriginalLanguage(Language newOriginalLanguage){
+    _updateWith(original: newOriginalLanguage);
+  }
+
+  void updateTranslatedLanguage(Language newTranslatedLanguage){
+    _updateWith(translated: newTranslatedLanguage);
+  }
+
+  void switchLanguages(){
+    _updateWith(original: translatedLanguage, translated: originalLanguage);
+  }
+
+  void _updateWith({ Language original, Language translated}){
+   this.originalLanguage = original;
+   this.translatedLanguage = translated;
+   notifyListeners();
+  }
+
+}

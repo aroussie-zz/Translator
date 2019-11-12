b403881efd111908264abbf17953fabf554c7a6a
Alex Roussiere <alex.roussiere@snapsheet.me>
2019-11-11 21:30:59 -0600

    Cards transition is working

--- lib/models/Quiz.dart
+++ lib/models/Quiz.dart
@@ -8,29 +8,18 @@ class Quiz {
 
   Quiz.dummy(){
     title = "Dummy Quiz";
-    questions = [
-      QuizQuestion(
-        question: "Question 1",
-        answers: [
-          QuizAnswer(answer: "First answer", isRightAnswer: false),
-          QuizAnswer(answer: "second answer", isRightAnswer: false),
-          QuizAnswer(answer: "third answer", isRightAnswer: true),
-          QuizAnswer(answer: "fourth answer", isRightAnswer: false)
-        ]
-      ),
-      QuizQuestion(
-          question: "Question 2",
+    for (int i = 0; i < 5; i++){
+      questions.add(QuizQuestion(
+          question: "Question $i",
           answers: [
-            QuizAnswer(answer: "First answer", isRightAnswer: true),
+            QuizAnswer(answer: "First answer", isRightAnswer: false),
             QuizAnswer(answer: "second answer", isRightAnswer: false),
-            QuizAnswer(answer: "third answer", isRightAnswer: false),
+            QuizAnswer(answer: "third answer", isRightAnswer: true),
             QuizAnswer(answer: "fourth answer", isRightAnswer: false)
           ]
-      )
-    ];
+      ));
+    }
   }
-
-
 }
 
 

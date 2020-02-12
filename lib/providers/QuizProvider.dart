16061ac9b88e996ffa0c6d6f291d43b98d168b7e
Alex Roussiere <alex.roussiere@snapsheet.me>
2020-02-11 22:33:00 -0600

    Need to find a way to trigger the fetching from providerModel

--- lib/providers/QuizProvider.dart
+++ lib/providers/QuizProvider.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:myTranslator/models/Quiz.dart';
+import 'package:myTranslator/utilities/DatabaseHelper.dart';
 
 class QuizProvider extends ChangeNotifier {
+
+  List<QuizModel> quizzes = [];
+  QuizModel currentQuiz;
 
   bool isQuizOver = false;
   bool hasAnswered = false;
@@ -13,19 +17,69 @@ class QuizProvider extends ChangeNotifier {
 
   int get totalQuestions => quiz.questions.length;
 
-  void updateQuizOver(bool isOver){
+  Future<List<QuizModel>> fetchQuizzes() async {
+    DatabaseHelper databaseHelper = DatabaseHelper();
+    List<Quiz> fetchedQuizzes = await databaseHelper.fetchQuizzes();
+    for (Quiz quiz in fetchedQuizzes){
+      this.quizzes.add(QuizModel(quiz: quiz));
+    }
+    notifyListeners();
+    return this.quizzes;
+  }
+
+  void setQuizUserActionVisibility(QuizModel quizModel, displayUserAction){
+    QuizModel quiz = this.quizzes.elementAt(this.quizzes.indexOf(quizModel));
+    quiz.updateWith(shouldDisplayUserAction: displayUserAction);
+    notifyListeners();
+  }
+
+//  void updateQuizList(List<Quiz> quizzes) {
+//    quizzes.map((quiz) =>
+//        this.quizzes.add(QuizModel(quiz: quiz))
+//    );
+//    notifyListeners();
+//  }
+
+  void updateQuizOver(bool isOver) {
     isQuizOver = isOver;
     notifyListeners();
   }
 
-  void updateHasAnswered(bool didAnswer){
+  void updateHasAnswered(bool didAnswer) {
     hasAnswered = didAnswer;
     notifyListeners();
   }
 
-  void increaseRightAnswerCount(){
+  void increaseRightAnswerCount() {
     rightAnswersCount++;
     notifyListeners();
+  }
+
+}
+
+class QuizModel {
+
+  Quiz quiz;
+  bool displayUserAction;
+  bool isQuizOver;
+  bool hasAnswered;
+  int rightAnswersCount;
+
+  QuizModel(
+      {@required this.quiz, this.displayUserAction = false, this.isQuizOver = false,
+        this.hasAnswered = false, this.rightAnswersCount = 0});
+
+  void updateWith({Quiz quiz, bool shouldDisplayUserAction, bool isQuizOver,
+    bool userHasAnswered, int rightAnswersCount}) {
+    this.quiz = quiz ?? this.quiz;
+    this.displayUserAction = shouldDisplayUserAction ?? this.displayUserAction;
+    this.isQuizOver = isQuizOver ?? this.isQuizOver;
+    this.hasAnswered = userHasAnswered ?? this.hasAnswered;
+    this.rightAnswersCount = rightAnswersCount ?? this.rightAnswersCount;
+  }
+
+  void increaseRightAnswersCount() {
+    this.rightAnswersCount++;
   }
 
 }
--- lib/screens/QuizListPage.dart
+++ lib/screens/QuizListPage.dart
@@ -3,70 +3,120 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:myTranslator/customWidget/PlatformDialog.dart';
 import 'package:myTranslator/models/Quiz.dart';
+import 'package:myTranslator/providers/QuizProvider.dart';
 import 'package:myTranslator/utilities/Constants.dart';
 import 'package:myTranslator/utilities/DatabaseHelper.dart';
+import 'package:provider/provider.dart';
 
-class QuizListPage extends StatefulWidget {
-  @override
-  _QuizListState createState() {
-    return _QuizListState();
-  }
-}
+//class QuizListPage extends StatefulWidget {
+//  @override
+//  _QuizListState createState() {
+//    return _QuizListState();
+//  }
+//}
 
-class _QuizListState extends State<QuizListPage> {
+class QuizListPage extends StatelessWidget {
+
   final GlobalKey<ScaffoldState> _key = GlobalKey<ScaffoldState>();
-  List<Quiz> _quizzes;
-  bool inEditMode = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: _key,
-      appBar: AppBar(title: Text("My Quizzes")),
-      body: SafeArea(
-          child: FutureBuilder(
-              future: _fetchQuizzed(),
-              builder: (context, snapshot) {
-                _quizzes = snapshot.data;
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: (_quizzes == null &&
-                          snapshot.connectionState != ConnectionState.done)
-                      ? Center(child: CircularProgressIndicator())
-                      //TODO: Seperate the quizzes == null scenario and display an Snackbar error instead
-                      : (_quizzes != null && _quizzes.isNotEmpty)
-                          ? StaggeredGridView.countBuilder(
-                              crossAxisCount: 2,
-                              mainAxisSpacing: 8.0,
-                              crossAxisSpacing: 8,
-                              itemCount: _quizzes.length,
-                              itemBuilder: (BuildContext context, int index) =>
-                                  _buildQuizCard(context, _quizzes[index]),
-                              staggeredTileBuilder: (int index) {
-                                return StaggeredTile.count(1, 1);
-                              })
-                          : Center(child: Text("No saved Quizzes")),
-                );
-              })),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add), onPressed: () => _onAddQuizClicked(context)),
+    return ChangeNotifierProvider<QuizProvider>(
+      builder: (context) => QuizProvider(),
+      child: Consumer<QuizProvider>(
+          builder: (context, model, child) =>
+              Scaffold(
+                key: _key,
+                appBar: AppBar(title: Text("My Quizzes")),
+                body: SafeArea(
+                    child: FutureBuilder(
+                        future: model.fetchQuizzes(),
+                        builder: (context, snapshot) {
+//                          _quizzes = snapshot.data;
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: (snapshot.data == null &&
+                                snapshot.connectionState !=
+                                    ConnectionState.done)
+                                ? Center(child: CircularProgressIndicator())
+                            //TODO: Seperate the quizzes == null scenario and display an Snackbar error instead
+                                : (model.quizzes != null &&
+                                model.quizzes.isNotEmpty)
+                                ? StaggeredGridView.countBuilder(
+                                crossAxisCount: 2,
+                                mainAxisSpacing: 8.0,
+                                crossAxisSpacing: 8,
+                                itemCount: model.quizzes.length,
+                                itemBuilder: (BuildContext context,
+                                    int index) =>
+                                    _buildQuizCard(context, model.quizzes[index]),
+                                staggeredTileBuilder: (int index) {
+                                  return StaggeredTile.count(1, 1);
+                                })
+                                : Center(child: Text("No saved Quizzes")),
+                          );
+                        })),
+                floatingActionButton: FloatingActionButton(
+                    child: Icon(Icons.add),
+                    onPressed: () => _onAddQuizClicked(context)),
+              )
+      ),
     );
   }
 
-  Widget _buildQuizCard(BuildContext context, Quiz quiz) {
+//class _QuizListState extends State<QuizListPage> {
+//  List<Quiz> _quizzes;
+//  bool inEditMode = false;
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      key: _key,
+//      appBar: AppBar(title: Text("My Quizzes")),
+//      body: SafeArea(
+//          child: FutureBuilder(
+//              future: _fetchQuizzed(),
+//              builder: (context, snapshot) {
+//                _quizzes = snapshot.data;
+//                return Padding(
+//                  padding: const EdgeInsets.all(8.0),
+//                  child: (_quizzes == null &&
+//                          snapshot.connectionState != ConnectionState.done)
+//                      ? Center(child: CircularProgressIndicator())
+//                      //TODO: Seperate the quizzes == null scenario and display an Snackbar error instead
+//                      : (_quizzes != null && _quizzes.isNotEmpty)
+//                          ? StaggeredGridView.countBuilder(
+//                              crossAxisCount: 2,
+//                              mainAxisSpacing: 8.0,
+//                              crossAxisSpacing: 8,
+//                              itemCount: _quizzes.length,
+//                              itemBuilder: (BuildContext context, int index) =>
+//                                  _buildQuizCard(context, _quizzes[index]),
+//                              staggeredTileBuilder: (int index) {
+//                                return StaggeredTile.count(1, 1);
+//                              })
+//                          : Center(child: Text("No saved Quizzes")),
+//                );
+//              })),
+//      floatingActionButton: FloatingActionButton(
+//          child: Icon(Icons.add), onPressed: () => _onAddQuizClicked(context)),
+//    );
+//  }
+
+  Widget _buildQuizCard(BuildContext context, QuizModel quizModel) {
     return new GestureDetector(
-        onTap: () => _startQuiz(quiz),
+        onTap: () => _startQuiz(quizModel.quiz, context),
         child: GestureDetector(
-            onLongPress: () => _showActionsLayout(context),
+            onLongPress: () => _showActionsLayout(context, quizModel),
             child: Card(
-                color: inEditMode ? Colors.black54 : Colors.white,
+                color: quizModel.displayUserAction ? Colors.black54 : Colors.white,
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(8)),
                     side: BorderSide(color: Colors.black, width: 1)),
                 child: Stack(
                   children: <Widget>[
-                    _buildQuizDisplay(context, quiz),
-                    inEditMode ? _buildActionsDisplay(context, quiz) : Container()
+                    _buildQuizDisplay(context, quizModel.quiz),
+                    quizModel.displayUserAction ? _buildActionsDisplay(context, quizModel) : Container()
                   ],
                 ))));
   }
@@ -91,7 +141,7 @@ class _QuizListState extends State<QuizListPage> {
     );
   }
 
-  Widget _buildActionsDisplay(BuildContext context, Quiz quiz) {
+  Widget _buildActionsDisplay(BuildContext context, QuizModel quizModel) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: <Widget>[
@@ -115,7 +165,7 @@ class _QuizListState extends State<QuizListPage> {
                     color: Colors.white,
                     child: IconButton(
                       iconSize: 34,
-                      onPressed: () => _onDeleteQuizClicked(context, quiz),
+                      onPressed: () => _onDeleteQuizClicked(context, quizModel),
                       color: Colors.black,
                       icon: Icon(Icons.delete),
                     )))
@@ -127,7 +177,7 @@ class _QuizListState extends State<QuizListPage> {
                 color: Colors.white,
                 child: IconButton(
                   iconSize: 34,
-                  onPressed: () => _onCancelClicked(context),
+                  onPressed: () => _onCancelClicked(context, quizModel),
                   color: Colors.red,
                   icon: Icon(Icons.cancel),
                 )))
@@ -135,38 +185,39 @@ class _QuizListState extends State<QuizListPage> {
     );
   }
 
-  void _showActionsLayout(BuildContext context) {
-    setState(() {
-      inEditMode = true;
-    });
+  void _showActionsLayout(BuildContext context, QuizModel quizModel) {
+    Provider.of<QuizProvider>(context).setQuizUserActionVisibility(quizModel, true);
+//    setState(() {
+//      inEditMode = true;
+//    });
   }
 
-  void _onDeleteQuizClicked(BuildContext context, Quiz quiz){
+  void _onDeleteQuizClicked(BuildContext context, QuizModel quizModel){
     PlatformDialog(
       titleWidget: Text("Delete quiz"),
-      content: Text("Are you sure you want to delete ${quiz.title}?"),
+      content: Text("Are you sure you want to delete ${quizModel.quiz.title}?"),
       defaultActionText: "Delete",
-      defaultAction: () => _deleteQuiz(quiz),
+      defaultAction: () => _deleteQuiz(quizModel, context),
       negativeActionText: "Cancel",
       negativeAction: () => Navigator.of(context, rootNavigator: true).pop(),
     ).show(context);
   }
 
-  void _deleteQuiz(Quiz quiz) async {
+  void _deleteQuiz(QuizModel quizModel, BuildContext context) async {
     var database = DatabaseHelper();
-    int success = await database.deleteQuiz(quiz);
+    //TODO: PUT THAT IN PROVIDER
+    int success = await database.deleteQuiz(quizModel.quiz);
     Navigator.of(context, rootNavigator: true).pop();
 
     if (success != 0) {
       final snackbar = SnackBar(
-        content: Text("${quiz.title} has been deleted successfully",
+        content: Text("${quizModel.quiz.title} has been deleted successfully",
             style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.green,
           duration: Duration(seconds: 2)
       );
       _key.currentState.showSnackBar(snackbar);
-      //TODO USE A PROVIDER INSTEAD TO REFRESH THE LIST
-      setState(() {inEditMode = false;});
+      Provider.of<QuizProvider>(context).setQuizUserActionVisibility(quizModel, false);
     } else {
       final snackbar = SnackBar(
         content: Text("Something went wrong",
@@ -179,17 +230,18 @@ class _QuizListState extends State<QuizListPage> {
 
   void _onEditQuizClicked(BuildContext context) {}
 
-  void _onCancelClicked(BuildContext context) {
-    setState(() {
-      inEditMode = false;
-    });
+  void _onCancelClicked(BuildContext context, QuizModel quizModel) {
+    Provider.of<QuizProvider>(context).setQuizUserActionVisibility(quizModel, false);
+//    setState(() {
+//      inEditMode = false;
+//    });
   }
 
   void _onAddQuizClicked(BuildContext context) {
     Navigator.of(context).pushNamed(createQuizRoute);
   }
 
-  void _startQuiz(Quiz quiz) {
+  void _startQuiz(Quiz quiz, BuildContext context) {
     Navigator.pushNamed(context, quizRoute, arguments: quiz);
   }
 
